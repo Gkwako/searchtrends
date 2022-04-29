f48569725fdc40a756aed6ff968dfbcff294ad12
gloriakwako <gloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nl>
2022-04-29 11:43:35 +0200

    new change 29

---
+++ script/script.js
@@ -0,0 +1,14 @@
+
+console.log(" sticky nav ");
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.querySelector("nav").style.top = "0";
+//   } else {
+//     document.querySelector("nav").style.top = "-100px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
