c8df7dc80dfedb89ca16a380ab2518a50f2c7e98
gloriakwako <gloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nl>
2022-05-05 16:54:39 +0200

    big change

--- script/script.js
+++ script/script.js
@@ -1,14 +1,25 @@
 
-console.log(" sticky nav ");
+console.log(" hamburger menu ");
 
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.querySelector("nav").style.top = "0";
-//   } else {
-//     document.querySelector("nav").style.top = "-100px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
+var menu = document.querySelector(".menu");
+var menuItems = document.querySelectorAll(".menuItem");
+var hamburger= document.querySelector(".ham");
+
+let open = 0;
+
+function toggleMenu() {
+  if (open == 0) {
+    open = 1; 
+    document.getElementById("menu").style.display = "none";
+    hamburger.classList.remove('active');
+    console.log(" close ");
+
+  } else {
+    open = 0;
+    document.getElementById("menu").style.display = "unset";
+    hamburger.classList.add('active');
+    console.log(" open ");
+  }
+}
+hamburger.addEventListener("click", toggleMenu);
 
