3b8753bbde2a73d3ecde0adaefa8941a8b234609
gloriakwako <gloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nlgloria.kwako@hva.nl>
2022-06-03 13:03:30 +0200

    .

--- script/script.js
+++ script/script.js
@@ -11,76 +11,26 @@ var linkclose = document.querySelector(".menu a");
 
 menuButton.addEventListener("click", toggleHamburgerMenu);
 
-// test = 0;
-
-
-// function toggleHamburgerMenu(){
-// 	if (test == 0){
-// 		hamburgerMenu.classList.add("visible");
-// 		test = 1;
-// 		menuButton.classList.toggle('.hamRotate.active');
-// 		console.log('visible');
-// 	} else if (test == 2){
-// 		hamburgerMenu.classList.remove("visible");
-// 		test = 0;
-// 		console.log('no visible');
-// 	}
-//     hamburgerMenu.classList.toggle("visible");
-// }
-// menuButton.addEventListener("click", toggleHamburgerMenu);
-
-// test = 0;
 
 
 function toggleHamburgerMenu(){
 	hamburgerMenu.classList.toggle('visible');
-
 }
 
 menuButton.addEventListener("click", toggleHamburgerMenu);
 
 
 
-
-
+// a IN HAMBURGER MENU TOGGLE IN PAGINA -----
 $( '.menu a' ).on("click", function(){
-	$('.icon').click();
+	$('button').click();
+	$('.ham').click();
 	console.log(" menu gone");
 
   });
 
 
  
-
-
-  // $('.ham').toggle('.hamRotate');
-	// console.log(" button toggled");
-
-
-
-// var menu = document.querySelector(".menu");
-// var menuItems = document.querySelectorAll(".menuItem");
-// var hamburger = document.querySelector(".ham");
-
-// let open = 0;
-
-// function toggleMenu() {
-// 	if (open == 0) {
-// 		open = 1;
-// 		document.getElementById("menu").style.display = "none";
-// 		hamburger.classList.remove('active');
-// 		console.log(" close ");
-
-// 	} else {
-// 		open = 0;
-// 		document.getElementById("menu").style.display = "unset";
-// 		hamburger.classList.add('active');
-// 		console.log(" open ");
-// 	}
-// }
-// hamburger.addEventListener("click", toggleMenu);
-
-
 
 // CAROUSEL -----
 console.log(" carousel ");
